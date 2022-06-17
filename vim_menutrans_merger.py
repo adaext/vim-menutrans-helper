9e225b65b0cc862205b23a1bee714d0e8c9d9d7d
Ada <me@yuhaowen.com>
2022-06-17 23:40:33 +0800

    replace the translations in dymmy template file with specified translation file

--- vim_menutrans_merger.py
+++ vim_menutrans_merger.py
@@ -10,8 +10,8 @@ def make_translated_dict(translation_file, translated_dict):
 
     # Encode with "latin1", because we don't care about the correctness of 
     # non-ASCII character.
-    with open(translation_file, encoding='UTF-8') as f2:
-        for line_number, line in enumerate(f2):
+    with open(translation_file, encoding='UTF-8') as f:
+        for line_number, line in enumerate(f):
             line = line.strip()
 
             if not line:
@@ -30,34 +30,71 @@ def make_translated_dict(translation_file, translated_dict):
             elif len(new_word_list) > 1 and new_word_list[0].startswith("g:menutrans_"):
                 translated_dict[new_word_list[0].lower()] = (
                         line_number, translation_file, new_word_list[0], new_word_list[2])
+        f.close()
 
-def extract_translated_message(translation_file):
+def extract_translated_message(template_file, translation_file):
     """Get the translated files' content and put it into translated_dict"""
 
     translated_dict = {}
     make_translated_dict(translation_file, translated_dict)
+    replace_template_translation(template_file, translated_dict)
 
-    print("translation_file_messages:")
-    for key in translated_dict.keys():
-        print(translated_dict[key][1] + ":" + 
-            str(translated_dict[key][0] + 1) + 
-            ": " + translated_dict[key][2] + 
-            ": " + translated_dict[key][3])
+def replace_template_translation(template_file, translated_dict):
+    """Replace the translations in template_file with the content in translated_dict"""
 
+    # Encode with "latin1", because we don't care about the correctness of 
+    # non-ASCII character.
+    with open(template_file, encoding='UTF-8') as f:
+        for line_number, line in enumerate(f):
+            line = line.strip()
+            if not line:
+                print("\n")
+                continue
+
+            # Split a line by white spaces but not backslash-escaped spaces.
+            new_word_list = re.findall(r"(?:\\ |[^ \t])+", line)
+            hasTranslated = True
+            
+            if len(new_word_list) > 1 and new_word_list[0] in MENUTRANS_COMMANDS:
+                if (new_word_list[1].lower() in translated_dict):
+                    new_word_list[2] = translated_dict[new_word_list[1].lower()][3]
+                    line = ' '.join(new_word_list)
+                else:
+                    hasTranslated = False
+            elif len(new_word_list) > 1 and new_word_list[0].startswith("g:menutrans_"):
+                if (new_word_list[0].lower() in translated_dict):
+                    new_word_list[2] = translated_dict[new_word_list[1].lower()][3]
+                    line = ' '.join(new_word_list)
+                else:
+                    hasTranslated = False
+            elif (len(new_word_list) > 1 and new_word_list[0] == "let" and 
+                    new_word_list[1].startswith("g:menutrans_")):
+                if (new_word_list[1].lower() in translated_dict):
+                    line = line[:line.find('=') + 2] + translated_dict[new_word_list[1].lower()][3]
+                else:
+                    hasTranslated = False
+            
+            if not hasTranslated:
+                line = '" ' + line
+            print(str(line_number + 1) + " " + line)
+        f.close()
 
 def usage():
-    print("""Usage: vim_menutrans_helper.py <translation_file>
+    print("""Usage: vim_menutrans_helper.py <template_file> <translation_file>
 
+    template_file: the menu translation file template, typically under 
+                      "$VIM-MENUTRANS-HELPER" directory.
     translation_file: the menu translation file to parse, typically under
                       "$VIMRUNTIME/lang" directory.
 """)
 
 def main():
-    if (not len(sys.argv) == 2 or not os.path.isfile(sys.argv[1])):
+    if (not len(sys.argv) == 3 or not os.path.isfile(sys.argv[1])
+            or not os.path.isfile(sys.argv[2])):
         usage()
         sys.exit(1)
 
-    extract_translated_message(sys.argv[1])
+    extract_translated_message(sys.argv[1], sys.argv[2])
 
 if __name__ == "__main__":
     main()
