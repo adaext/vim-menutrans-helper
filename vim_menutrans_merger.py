1888d1ebc19bf8b62066f935bd391cf799d81a32
Ada <me@yuhaowen.com>
2022-06-20 12:47:08 +0800

    format problems fixes and encoding change

--- vim_menutrans_merger.py
+++ vim_menutrans_merger.py
@@ -10,7 +10,7 @@ def make_translated_dict(translation_file, translated_dict):
 
     # Encode with "latin1", because we don't care about the correctness of 
     # non-ASCII character.
-    with open(translation_file, encoding='UTF-8') as f:
+    with open(translation_file, encoding='latin1') as f:
         for line_number, line in enumerate(f):
             line = line.strip()
 
@@ -23,14 +23,17 @@ def make_translated_dict(translation_file, translated_dict):
             
             if len(new_word_list) > 1 and new_word_list[0] in MENUTRANS_COMMANDS:
                 translated_dict[new_word_list[1].lower()] = (
-                        line_number, translation_file, new_word_list[1], line[match_index[1].end():])
+                        line_number, translation_file, new_word_list[1], 
+                        line[match_index[1].end():])
             elif len(new_word_list) > 1 and new_word_list[0].startswith("g:menutrans_"):
                 translated_dict[new_word_list[0].lower()] = (
-                        line_number, translation_file, new_word_list[0], line[match_index[0].end():])
+                        line_number, translation_file, new_word_list[0], 
+                        line[match_index[0].end():])
             elif (len(new_word_list) > 1 and new_word_list[0] == "let" and 
                     new_word_list[1].startswith("g:menutrans_")):
                 translated_dict[new_word_list[1].lower()] = (
-                        line_number, translation_file, new_word_list[1], line[match_index[2].end():])
+                        line_number, translation_file, new_word_list[1], 
+                        line[match_index[2].end():])
 
 def extract_translated_message(template_file, translation_file):
     """Get the translated files' content and put it into translated_dict"""
@@ -42,51 +45,54 @@ def extract_translated_message(template_file, translation_file):
 def replace_template_translation(template_file, translated_dict):
     """Replace the translations in template_file with the content in translated_dict"""
 
-    newContent = ""
+    new_content = ""
     # Encode with "latin1", because we don't care about the correctness of 
     # non-ASCII character.
-    with open(template_file, encoding='UTF-8') as f:
+    with open(template_file, encoding='latin1') as f:
         for line_number, line in enumerate(f):
             line = line.strip()
             if not line:
-                newContent += "\n"
+                new_content += "\n"
                 continue
 
             # Split a line by white spaces but not backslash-escaped spaces.
             new_word_list = re.findall(r"(?:\\ |[^ \t])+", line)
             match_index = [each for each in re.finditer(r"(?:\\ |[^ \t])+", line)]
-            hasTranslated = True
+            has_translated = True
             
             if len(new_word_list) > 1 and new_word_list[0] in MENUTRANS_COMMANDS:
                 if (new_word_list[1].lower() in translated_dict):
-                    line = line[:match_index[1].end() + 1] + translated_dict[new_word_list[1].lower()][3]
+                    line = line[:match_index[1].end() + 1] 
+                    + translated_dict[new_word_list[1].lower()][3]
                 else:
-                    hasTranslated = False
+                    has_translated = False
             elif len(new_word_list) > 1 and new_word_list[0].startswith("g:menutrans_"):
                 if (new_word_list[0].lower() in translated_dict):
-                    line = line[:match_index[0].end() + 1] + translated_dict[new_word_list[0].lower()][3]
+                    line = line[:match_index[0].end() + 1] 
+                    + translated_dict[new_word_list[0].lower()][3]
                 else:
-                    hasTranslated = False
+                    has_translated = False
             elif (len(new_word_list) > 1 and new_word_list[0] == "let" and 
                     new_word_list[1].startswith("g:menutrans_")):
                 if (new_word_list[1].lower() in translated_dict):
-                    line = line[:match_index[2].end() + 1] + translated_dict[new_word_list[1].lower()][3]
+                    line = line[:match_index[2].end() + 1] 
+                    + translated_dict[new_word_list[1].lower()][3]
                 else:
-                    hasTranslated = False
+                    has_translated = False
             
-            if not hasTranslated:
+            if not has_translated:
                 line = '" ' + line
 
-            newContent += str(line_number + 1) + line
-            newContent += "\n"
+            new_content += str(line_number) + line
+            new_content += "\n"
 
-    with open("test.vim", mode="w", encoding="UTF-8") as f:
-        f.write(newContent)
+    with open(template_file, mode="w", encoding="latin1") as f:
+        f.write(new_content)
 
 def usage():
-    print("""Usage: vim_menutrans_helper.py <template_file> <translation_file>
+    print("""Usage: vim_menutrans_merger.py <template_file> <translation_file>
 
-    template_file: the menu translation file template, typically under 
+    template_file:    the menu translation file template, typically under 
                       "$VIM-MENUTRANS-HELPER" directory.
     translation_file: the menu translation file to parse, typically under
                       "$VIMRUNTIME/lang" directory.
